9e63e6cc15d69c07313927c360dcf41d38d37036
Dan Nemeth <dnemeth@pivotal.io>
2016-01-24 20:30:10 -0500

    Added WEBROOT environment variable that defaults to current directory. Updated the static asset path to be WEBROOT-relative.

--- server.go
+++ server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 )
+
+var webRoot string
 
 // NewServer configures and returns a Server.
 func NewServer() *negroni.Negroni {
@@ -25,8 +28,18 @@ func NewServer() *negroni.Negroni {
 }
 
 func initRoutes(mx *mux.Router, formatter *render.Render) {
+	webRoot = os.Getenv("WEBROOT")
+	if len(webRoot) == 0 {
+		root, err := os.Getwd()
+		if err != nil {
+			panic("Could not retrieve working directory")
+		} else {
+			webRoot = root
+		}
+	}
+
 	mx.HandleFunc("/api/test", testHandler(formatter)).Methods("GET")
 	mx.HandleFunc("/cookies/write", cookieWriteHandler(formatter)).Methods("GET")
 	mx.HandleFunc("/cookies/read", cookieReadHandler(formatter)).Methods("GET")
-	mx.PathPrefix("/").Handler(http.FileServer(http.Dir("./assets/")))
+	mx.PathPrefix("/").Handler(http.FileServer(http.Dir(webRoot + "/assets/")))
 }
